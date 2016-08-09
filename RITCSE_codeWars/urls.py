bc43989d2fb940716bdc517fbf5119d2ebffae74
Sidhin <sidhin.thomas@gmail.com>
2016-08-10 01:56:36 +0530

    new views and edits

--- RITCSE_codeWars/urls.py
+++ RITCSE_codeWars/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
-from . import views
+from RITCSE_codeWars import views
+
 urlpatterns = [
-    url(r'^$', views.index, name="CodeWars"),
+    url(r'^$', views.index, name="Index"),
     url(r'^allsubmissions', views.all_submission, name="AllSubmissions"),
     url(r'^yoursubmission', views.your_submissions, name="YourSubmissions"),
     url(r'^code', views.your_code, name="YourCode"),
-    url(r'^registration', views.UserFormView.as_view(), name="Registration")
+    url(r'^registration', views.UserFormView.as_view(), name="Registration"),
+    url(r'^login/$', views.login_user, name="Login"),
+    url(r'authenticate/$', views.authenticate_user, name="AuthenticateUser"),
 ]
--- RITCSE_codeWars/views.py
+++ RITCSE_codeWars/views.py
@@ -1,12 +1,15 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 from django.views.generic import View
 from .form import UserForm
 
 
-
 def index(request):
-    return render(request,'RITCSE_codeWars/Home.html')
+    return render(request, 'RITCSE_codeWars/Home.html')
+
 
 def all_submission(request):
     return render(request, 'RITCSE_codeWars/AllSubmissions.html')
@@ -14,20 +17,40 @@ def all_submission(request):
 def your_submissions(request):
     return render(request, 'RITCSE_codeWars/YourSubmissions.html')
 
+
 def your_code(request):
-    return render(request,'RITCSE_codeWars/YourCode.html')
+    return render(request, 'RITCSE_codeWars/YourCode.html')
+
 
 class UserFormView(View):
-    form_class= UserForm
-    template_name= 'RITCSE_codeWars/registration.html'
+    form_class = UserForm
+    template_name = 'RITCSE_codeWars/registration.html'
 
-
-    def get(self,request):
+    def get(self, request):
         form = self.form_class(None)
-        return render(request, self.template_name,{'form':form})
+        return render(request, self.template_name, {'form': form})
 
-    def post(self,request):
+    def post(self, request):
         form = self.form_class(request.POST)
 
         if form.is_valid():
-            user = form.save(commit=True)
+            form.save(commit=True)
+
+
+def login_user(request):
+    return render('RITCSE_codeWars/Login.html')
+
+
+def authenticate_user(request):
+    try:
+        username = request.POST['username']
+        password = request.POST['password']
+    except KeyError:
+        return HttpResponseRedirect(reverse('Login'))
+    user = authenticate(username=username, password=password)
+
+    if user is None:
+        return HttpResponseRedirect(reverse('Login') + '?error=true')
+    login(request, user)
+    return HttpResponseRedirect(reverse('Index'))
+
